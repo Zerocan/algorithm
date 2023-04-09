f0540c5d8e884498a3a0190432a5859e4ae145b7
Zerocan <1285711756@qq.com>
2023-04-09 21:18:29 +0800

    子集 II

--- leetcode/90.js
+++ leetcode/90.js
@@ -9,7 +9,7 @@ var subsetsWithDup = function (nums) {
   const backtracking = (startIndex) => {
     res.push([...path]);
     for (let i = startIndex; i < nums.length; i++) {
-      // 代码随想录解法
+      // 代码随想录解法 i > startIndex 用来控制同一层去重，而不会在枝上去重
       if (i > startIndex && nums[i - 1] === nums[i]) {
         continue;
       }
