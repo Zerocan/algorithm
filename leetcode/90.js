f565af642b01aa77873265c4000cf813188dfa7c
Zerocan <1285711756@qq.com>
2023-04-09 19:53:33 +0800

    子集 II

---
+++ leetcode/90.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function (nums) {
+  const res = [];
+  const path = [];
+  nums = nums.sort((a, b) => a - b);
+  const backtracking = (startIndex) => {
+    res.push([...path]);
+    for (let i = startIndex; i < nums.length; i++) {
+      // 代码随想录解法
+      if (i > startIndex && nums[i - 1] === nums[i]) {
+        continue;
+      }
+      path.push(nums[i]);
+      backtracking(i + 1);
+      path.pop();
+      // while (nums[i + 1] === nums[i]) {
+      //   i++;
+      // }
+    }
+  };
+
+  backtracking(0);
+  return res;
+};
+
+console.log(subsetsWithDup([1, 2, 2]));
