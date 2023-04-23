256571c769139091b99ee7dca62eb1e6ba97d205
Zerocan <1285711756@qq.com>
2023-04-23 21:56:51 +0800

    零钱兑换 II

---
+++ leetcode/518.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function(amount, coins) {
+  const dp = new Array(amount + 1).fill(0);
+  dp[0] = 1;
+
+  for (let i = 0; i < coins.length; i++) {
+    for (let j = coins[i]; j <= amount; j++) {
+      dp[j] += dp[j-coins[i]];
+    }
+  }
+
+  return dp[amount];
+};
+
+console.log(change(3, [2]));
