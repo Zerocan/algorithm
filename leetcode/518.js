ff00e279b9b3fc7bf0d5c59284174af8c8f64b9c
Zerocan <1285711756@qq.com>
2023-04-23 22:16:18 +0800

    零钱兑换II

--- leetcode/518.js
+++ leetcode/518.js
@@ -7,8 +7,9 @@ var change = function(amount, coins) {
   const dp = new Array(amount + 1).fill(0);
   dp[0] = 1;
 
-  for (let i = 0; i < coins.length; i++) {
-    for (let j = coins[i]; j <= amount; j++) {
+  // 组合
+  for (let i = 0; i < coins.length; i++) { // 先物品
+    for (let j = coins[i]; j <= amount; j++) { // 后背包
       dp[j] += dp[j-coins[i]];
     }
   }
