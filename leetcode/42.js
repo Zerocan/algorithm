2b81b624bbe53fea25b62667274c47450b4e9487
Zerocan <1285711756@qq.com>
2023-08-07 21:14:46 +0800

    接雨水

--- leetcode/42.js
+++ leetcode/42.js
@@ -24,4 +24,31 @@ var trap = function (height) {
   return res;
 };
 
-console.log(trap([5,5,1,7,1,1,5,2,7,6]));
+console.log(trap([5, 5, 1, 7, 1, 1, 5, 2, 7, 6]));
+
+// 暴力解法
+var trap1 = function (height) {
+  const len = height.length;
+  let sum = 0;
+  for (let i = 0; i < len; i++) {
+    // 第一个柱子和最后一个柱子不接雨水
+    if (i == 0 || i == len - 1) continue;
+
+    let rightHeight = height[i];
+    let leftHeight = height[i];
+
+    for (let r = i + 1; r < len; r++) {
+      if (height[r] > rightHeight) rightHeight = height[r];
+    }
+    for (let l = i - 1; l >= 0; l--) {
+      if (height[l] > leftHeight) leftHeight = height[l];
+    }
+
+    const minHeight = Math.min(rightHeight, leftHeight);
+    sum += minHeight - height[i];
+  }
+
+  return sum;
+};
+
+console.log(trap1([5, 5, 1, 7, 1, 1, 5, 2, 7, 6]));
